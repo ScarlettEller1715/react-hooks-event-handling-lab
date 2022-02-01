0a6a7f9cff8c0501db3c5301fea2329c5f1b232d
Scarlett Eller <rq6715@gmail.com>
2022-02-01 16:19:23 -0500

    completed assignment

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,13 @@
-// Code EyesOnMe Component Here
+import React, { useState } from "react";
+
+function EyesOnMe() {
+    return (
+        <button 
+        onFocus={() => console.log('Good!')}
+        onBlur={() => console.log('All eyes on me!')}>
+            All eyes on me!
+        </button>
+    )
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,11 @@
-// Code Keypad Component Here
+import React, {useState} from "react"
+
+function Keypad() {
+    return (<form>
+        <input 
+        type="password"
+        onChange={(e) => console.log("entering password")} />
+        </form>)
+}
+
+export default Keypad;
